adf82b25e2d46c2e67cad0227b79f02b2a2b6e90
John Andrew Loudon <blue-eyeswhitedragon@users.noreply.github.com>
2024-10-30 14:31:33 +0000

    Audient iD Series Cask
    Zap files adjusted
    
    DRY backup checks, acknowledgement of checksum change, regex update
    
    single livecheck to avoid throughput throttling from audient servers

---
+++ Casks/audient-id.rb
@@ -0,0 +1,53 @@
+cask "audient-id" do
+  on_el_capitan :or_older do
+    version "3.2.0"
+    sha256 "b39fc04f144c5305d73fc6ae626d5b3bfb5e79e00550f90c49c554e514dcf477"
+
+    url "https://d9w4fhj63j193.cloudfront.net/2021/Legacy%20Drivers/iD%20Legacy%20Drivers%20Mac/iD%20v#{version}.dmg",
+        verified: "d9w4fhj63j193.cloudfront.net/"
+
+    livecheck do
+      skip "Legacy Version"
+    end
+  end
+  on_sierra do
+    version "4.04"
+    sha256 "a0e476b3bf10b827f1b38a4fffacbcca1873cd979f3b89d4f630356d283d1baf"
+
+    url "https://d9w4fhj63j193.cloudfront.net/2021/Legacy%20Drivers/iD%20Legacy%20Drivers%20Mac/iD%20v#{version}.dmg",
+        verified: "d9w4fhj63j193.cloudfront.net/"
+
+    livecheck do
+      skip "Legacy Version"
+    end
+  end
+  on_high_sierra :or_newer do
+    live_search = %r{href=.*?/iD(?:%20|\s)*v?(\d+(?:\.\d+)*(?:b\d+)?)\.dmg}i
+    year = 2024
+
+    version "4.4.0b1"
+    sha256 "cb213a35c84d63ca5fda01e3f944447ae2e95ee1efb8de8c0efcf44b84bc2bce"
+
+    url "https://d9w4fhj63j193.cloudfront.net/#{year}/iD%20Drivers/iD%20v#{version}.dmg",
+        verified: "d9w4fhj63j193.cloudfront.net/"
+
+    livecheck do
+      url "https://audient.com/products/audio-interfaces/id4/downloads/"
+      regex(live_search)
+    end
+  end
+
+  name "Audient ID Drivers"
+  desc "CoreAudio compatible drivers for the Audient ID range, including MKII"
+  homepage "https://audient.com/id-audio-interface-range/"
+
+  depends_on macos: ">= :el_capitan"
+
+  app "iD.app"
+
+  zap trash: [
+    "~/Library/Application Support/Audient",
+    "~/Library/Preferences/com.audient.id.plist",
+    "~/Library/Saved Application State/com.audient.id.savedState",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,34 @@
+# README
+
+This for System Administrators that use Audient software within the studio and wish to track updates via Homebrew. It is useful for updating, resetting, or upgrading hardware on downtime days.
+
+Supports Legacy Versions from El Capitan for the iD, <strike>Sono</strike>, and <strike>Evo</strike> product ranges.
+
+# How to install
+
+```zsh
+brew tap blue-eyeswhitedragon/audient
+```
+
+### iD
+
+```zsh
+brew install --cask audient-id
+```
+
+<!-- ### Sono
+
+```zsh
+brew install --cask audient-sono
+```
+
+### Evo
+
+```zsh
+brew install --cask audient-evo
+``` -->
+
+## Note / Caveats
+
+The iD4 MKI was discontinued on Jan 2021 with the launch if the iD4 MKII.
+Audient's <a href="https://support.audient.com/hc/en-us/categories/201449766-iD4-MKI">support</a> for the device still points at the iD software in general, so you should be fine. If not, click on the support link provided previously in this paragraph.
